6674e1f814674e827bf0a8d69b55b4d4c681611f
Hayden <haydendonald@gmail.com>
2017-03-03 16:37:20 +1100

    Update

--- UpstreamKeyer.cs
+++ UpstreamKeyer.cs
@@ -20,15 +20,20 @@ namespace ATEMVisionSwitcher
         public String Id { get { return _id; } }
         public IBMDSwitcherKey Object { get { return _object; } }
         public UpstreamKeyerMonitor Monitor { get { return _monitor; } }
-        public bool isOnAir
-        {
-            get
-            {
-                int state = 0;
-                _object.GetOnAir(out state);
-                return (state == 1);
-            }
-        }
+        public bool CanBeDVEPool { get { int value; _object.CanBeDVEKey(out value); return value == 1; } }
+        public _BMDSwitcherInputAvailability CutInputAvailabilityMask { get { _BMDSwitcherInputAvailability value; _object.GetCutInputAvailabilityMask(out value); return value; } }
+        public _BMDSwitcherInputAvailability FillInputAvailabilityMask { get { _BMDSwitcherInputAvailability value; _object.GetFillInputAvailabilityMask(out value); return value; } }
+        public int HashCode { get { return _object.GetHashCode(); } }
+        public long InputCut { get { long value; _object.GetInputCut(out value); return value; } }
+        public long InputFill { get { long value; _object.GetInputFill(out value); return value; } }
+        public double MaskBottom { get { double value; _object.GetMaskBottom(out value); return value; } }
+        public Boolean Masked { get { int value; _object.GetMasked(out value); return value == 1; } }
+        public double MaskLeft { get { double value; _object.GetMaskLeft(out value); return value; } }
+        public double MaskRight { get { double value; _object.GetMaskRight(out value); return value; } }
+        public double MaskTop { get { double value; _object.GetMaskTop(out value); return value; } }
+        public _BMDSwitcherTransitionSelection TransitionSelectionMask { get { _BMDSwitcherTransitionSelection value; _object.GetTransitionSelectionMask(out value); return value; } }
+        public _BMDSwitcherKeyType Type { get { _BMDSwitcherKeyType value; _object.GetType(out value); return value; } }
+        public _BMDSwitcherKeyType ResetMask { get { _BMDSwitcherKeyType value; _object.GetType(out value); return value; } }
 
         //Constructor
         public UpstreamKeyer(DebugConsole console, IBMDSwitcherKey keyer, long number, String id = "Not Set")
@@ -38,24 +43,28 @@ namespace ATEMVisionSwitcher
             _number = number;
             _id = id;
 
+   
+            _object.SetMaskLeft;
+            _object.SetMaskRight;
+            _object.SetMaskTop;
+            _object.SetOnAir;
+            _object.SetType;
+
+
+
             //Add the monitors to the callback
-            _monitor = new UpstreamKeyerMonitor();
+            _monitor = new UpstreamKeyerMonitor(Console, id, number);
             _object.AddCallback(_monitor);
 
             Console.sendWarning("Upstream Keyer Does Not Contain All Methods");
         }
 
-        //Deconstructor
-        ~UpstreamKeyer()
-        {
-            try
-            {
-                _object.RemoveCallback(_monitor);
-                _monitor = new UpstreamKeyerMonitor();
-                _object = null;
-            }
-            catch(Exception e) { Console.sendError("Could Not Release Upstream Keyer " + Id + "\nMore Information:\n" + e); }
-        }
+        public Boolean SetInputCut(long value) { try { _object.SetInputCut(value); return true; } catch (Exception e) { Console.sendError("Could Not Set Input Cut To Upstream Keyer " + _id + " (" + _number + ")\nMore Information:\n" + e); return false; } }
+        public Boolean SetInputFill(long value) { try { _object.SetInputFill(value); return true; } catch (Exception e) { Console.sendError("Could Not Set Input Fill To Upstream Keyer " + _id + " (" + _number + ")\nMore Information:\n" + e); return false; } }
+        public Boolean SetMaskBottom(long value) { try { _object.SetMaskBottom(value); return true; } catch (Exception e) { Console.sendError("Could Not Set Mask Bottom To Upstream Keyer " + _id + " (" + _number + ")\nMore Information:\n" + e); return false; } }
+        public Boolean SetMasked(int value) { try { _object.SetMasked(value); return true; } catch (Exception e) { Console.sendError("Could Not Set Masked To Upstream Keyer " + _id + " (" + _number + ")\nMore Information:\n" + e); return false; } }
+
+
 
         //Set the keyers state to on air
         public Boolean SetOnAir(Boolean value = true)
