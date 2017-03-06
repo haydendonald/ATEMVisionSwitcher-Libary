b1279f3a163dd542df2a3e012e2de25524c8f233
Hayden <haydendonald@gmail.com>
2017-03-06 15:37:33 +1100

    Finished AuxInputMonitor
    I think. Theres no more methods required by the SDK but i haven't added
    events using the _BMDSwitcherInputCallback, only the
    _BMDSwitcherInputAuxCallback

---
+++ Monitors/AuxInputMonitor.cs
@@ -0,0 +1,47 @@
+﻿/**
+	ATEM Vision Switcher Libary By Hayden Donald 2017
+	https://github.com/haydendonald/ATEMVisionSwitcher-Libary
+
+	This libary is repsonsible for the interfacing with the Black Magic ATEM Vision Switcher using the given api
+    found at https://www.blackmagicdesign.com/support
+*/
+
+using System;
+using BMDSwitcherAPI;
+
+namespace ATEMVisionSwitcher.Monitors
+{
+    public class AuxInputMonitor : IBMDSwitcherInputAuxCallback
+    {
+        private DebugConsole Console;
+        private String _longName;
+        private long _id;
+
+        //Constructor
+        public AuxInputMonitor(DebugConsole console, String longName, long id)
+        {
+            Console = console;
+            _id = id;
+            _longName = longName;
+
+            Console.sendVerbose("Created AuxInputMonitor Object For Input " + longName + " (" + id + ")");
+        }
+
+        public event EventHandler InputSourceChanged;
+
+        public void Notify(_BMDSwitcherInputAuxEventType eventType)
+        {
+            //Switch the event type
+            switch (eventType)
+            {
+                case _BMDSwitcherInputAuxEventType.bmdSwitcherInputAuxEventTypeInputSourceChanged:
+                    if (InputSourceChanged != null)
+                    {
+                        Console.sendVerbose("InputSource Has Changed On Switcher Input " + _longName + " (" + _id + ")");
+                        InputSourceChanged(this, null);
+                    }
+                    break;
+            }
+        }
+    }
+}
