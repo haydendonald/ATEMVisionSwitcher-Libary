9ce133bc6f2986c08a1a5503040d9779f6c17653
Hayden <haydendonald@gmail.com>
2017-03-14 09:37:57 +1100

    Added functionality in the Inputs class
    Added switcher input passes to the inputs class

--- Inputs.cs
+++ Inputs.cs
@@ -19,9 +19,115 @@ namespace ATEMVisionSwitcher
         private List<SwitcherInput> _switcherInputs;
         private List<AuxInput> _auxInputs;
 
-        //Properties
+        #region Global Properties
+
         public List<SwitcherInput> SwitcherInputs { get { return _switcherInputs; } }
         public List<AuxInput> AuxInputs { get { return _auxInputs; } }
+
+        //Id
+        public long GetId(Input input) { return input.Id; }
+        public List<long> GetId(List<SwitcherInput> inputs)
+        {
+            List<long> returnList = new List<long> { };
+            foreach (Input i in inputs) { returnList.Add(i.Id); }
+            return returnList;
+        }
+
+        //LongName
+        public String GetLongName(Input input) { return input.LongName; }
+        public List<String> GetLongName(List<Input> inputs)
+        {
+            List<String> returnList = new List<String> { };
+            foreach (Input i in inputs) { returnList.Add(i.LongName); }
+            return returnList;
+        }
+        public void SetCurrentExternalPortType(Input input, String value) { input.LongName = value; }
+        public void SetCurrentExternalPortType(List<Input> inputs, String value) { foreach (Input i in inputs) { i.LongName = value; } }
+
+
+        //ShortName
+        public String GetShortName(Input input) { return input.ShortName; }
+        public List<String> GetShortName(List<Input> inputs)
+        {
+            List<String> returnList = new List<String> { };
+            foreach (Input i in inputs) { returnList.Add(i.ShortName); }
+            return returnList;
+        }
+        public void SetShortName(Input input, String value) { input.ShortName = value; }
+        public void SetShortName(List<Input> inputs, String value) { foreach (Input i in inputs) { i.ShortName = value; } }
+
+        #endregion
+
+        #region SwitcherInput Properties
+
+        //AvailableExternalPortTypes
+        public _BMDSwitcherExternalPortType GetAvailableExternalPortTypes(SwitcherInput input) { return input.AvailableExternalPortTypes; }
+        public List<_BMDSwitcherExternalPortType> GetAvailableExternalPortTypes(List<SwitcherInput> inputs)
+        {
+            List<_BMDSwitcherExternalPortType> returnList = new List<_BMDSwitcherExternalPortType> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.AvailableExternalPortTypes); }
+            return returnList;
+        }
+        public void SetAvailableExternalPortTypes(Keyer keyer, Boolean value = true) { keyer.OnAir = value; }
+        public void SetAvailableExternalPortTypes(List<Keyer> keyers, Boolean value) { foreach (Keyer i in keyers) { i.OnAir = value; } }
+
+        //CurrentExternalPortType
+        public _BMDSwitcherExternalPortType GetCurrentExternalPortType(SwitcherInput input) { return input.CurrentExternalPortType; }
+        public List<_BMDSwitcherExternalPortType> GetCurrentExternalPortType(List<SwitcherInput> inputs)
+        {
+            List<_BMDSwitcherExternalPortType> returnList = new List<_BMDSwitcherExternalPortType> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.CurrentExternalPortType); }
+            return returnList;
+        }
+        public void SetCurrentExternalPortType(SwitcherInput input, _BMDSwitcherExternalPortType value) { input.CurrentExternalPortType = value; }
+        public void SetCurrentExternalPortType(List<SwitcherInput> inputs, _BMDSwitcherExternalPortType value) { foreach (SwitcherInput i in inputs) { i.CurrentExternalPortType = value; } }
+
+        //InputAvailability
+        public _BMDSwitcherInputAvailability GetInputAvailability(SwitcherInput input) { return input.InputAvailability; }
+        public List<_BMDSwitcherInputAvailability> GetInputAvailability(List<SwitcherInput> inputs)
+        {
+            List<_BMDSwitcherInputAvailability> returnList = new List<_BMDSwitcherInputAvailability> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.InputAvailability); }
+            return returnList;
+        }
+
+        //PortType
+        public _BMDSwitcherPortType GetPortType(SwitcherInput input) { return input.PortType; }
+        public List<_BMDSwitcherPortType> GetPortType(List<SwitcherInput> inputs)
+        {
+            List<_BMDSwitcherPortType> returnList = new List<_BMDSwitcherPortType> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.PortType); }
+            return returnList;
+        }
+
+        //PreviewTallied
+        public Boolean GetPreviewTallied(SwitcherInput input) { return input.PreviewTallied; }
+        public List<Boolean> GetPreviewTallied(List<SwitcherInput> inputs)
+        {
+            List<Boolean> returnList = new List<Boolean> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.PreviewTallied); }
+            return returnList;
+        }
+
+        //PropgramTallied
+        public Boolean GetProgramTallied(SwitcherInput input) { return input.ProgramTallied; }
+        public List<Boolean> GetProgramTallied(List<SwitcherInput> inputs)
+        {
+            List<Boolean> returnList = new List<Boolean> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.ProgramTallied); }
+            return returnList;
+        }
+
+        //ResetNames
+        public Boolean ResetNames(SwitcherInput input) { return input.ResetNames(); }
+        public List<Boolean> ResetNames(List<SwitcherInput> inputs)
+        {
+            List<Boolean> returnList = new List<Boolean> { };
+            foreach (SwitcherInput i in inputs) { returnList.Add(i.ResetNames()); }
+            return returnList;
+        }
+
+        #endregion 
 
         public Inputs(ref DebugConsole debugConsole)
         {
